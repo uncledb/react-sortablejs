937cab0594b3a57c443112c5e1c3e25b93f60c22
uncle <434967007@qq.com>
2017-09-15 16:54:47 +0800

    搭建环境

--- src/main/SortableList.js
+++ src/main/SortableList.js
@@ -5,12 +5,17 @@ import Sortable from 'sortablejs';
 class SortableList extends React.Component {
 
 	componentDidMount() {
-
+		var el = document.getElementById('items');
+		var sortable = Sortable.create(el);
 	}
 
 	render() {
 		return (
-			<div>test123</div>
+			<ul id="items">
+				<li>item 1</li>
+				<li>item 2</li>
+				<li>item 3</li>
+			</ul>
 		)
 	}
 }
